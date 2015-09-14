76117d882e36d71fe8dcb46d5012600c6db54749
Duncan Smith <dunksmith@gmail.com>
2015-09-14 12:18:52 +0100

    Convert spaces to tabs for consistency

--- js/angular-dotdotdot.js
+++ js/angular-dotdotdot.js
@@ -1,19 +1,16 @@
 angular.module('dotdotdot-angular', [])
-	.directive('dotdotdot', ['$timeout', function($timeout){
-	    return {
-	        restrict: 'A',
-	        link: function(scope, element, attributes) {
+    .directive('dotdotdot', ['$timeout', function($timeout) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attributes) {
 
-	        	// We must pass in the scope binding, e.g. dotdotdot='myText' for scope.myText
-	            scope.$watch(attributes.dotdotdot, function() {
+                // We must pass in the scope binding, e.g. dotdotdot='myText' for scope.myText
+                scope.$watch(attributes.dotdotdot, function() {
 
-	            	// Wait for DOM to render
-	                $timeout(function() {
-
-	                	// Don't use { watch: true } as it polls continuously
-	                    element.dotdotdot();
-	                }, 400);
-	            });
-	        }
-	    }
+                    // Wait for DOM to render
+                    // NB. Don't use { watch: true } option in dotdotdot as it needlessly polls
+                    $timeout(element.dotdotdot, 400);
+                });
+            }
+        }
 }]);
