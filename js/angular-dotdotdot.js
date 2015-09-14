3bd8fcd3c0a8dd70927281f3bec142a265033cc1
Duncan Smith <dunksmith@gmail.com>
2015-09-14 12:11:01 +0100

    Avoid unnecessary calls to dotdotdot plugin, include example in README

--- js/angular-dotdotdot.js
+++ js/angular-dotdotdot.js
@@ -1,11 +1,19 @@
 angular.module('dotdotdot-angular', [])
-	.directive('dotdotdot', function(){
-		return {
-			restrict: 'A',
-			link: function(scope, element, attributes) {
-				scope.$watch(function() {
-					element.dotdotdot({watch: true});
-				});
-			}
-		}
-	});
+	.directive('dotdotdot', ['$timeout', function($timeout){
+	    return {
+	        restrict: 'A',
+	        link: function(scope, element, attributes) {
+
+	        	// We must pass in the scope binding, e.g. dotdotdot='myText' for scope.myText
+	            scope.$watch(attributes.dotdotdot, function() {
+
+	            	// Wait for DOM to render
+	                $timeout(function() {
+
+	                	// Don't use { watch: true } as it polls continuously
+	                    element.dotdotdot();
+	                }, 400);
+	            });
+	        }
+	    }
+}]);
